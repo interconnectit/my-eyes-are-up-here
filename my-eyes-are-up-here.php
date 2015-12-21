22287b23c159a2f1299ba8fe3e287bbeb2cdcf7a
Evgenii Nasyrov <inasyrov@yandex.ru>
2015-12-21 10:30:18 +0000

    Add index file

---
+++ my-eyes-are-up-here.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: My Eyes Are Up Here
+ * Plugin URI: https://github.com/interconnectit/my-eyes-are-up-here
+ * Description: Detects faces during thumbnail cropping and moves the crop position accordingly.
+ * Version: 1.0.1
+ * Author: interconnect/it
+ * Author URI: http://interconnectit.com
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
