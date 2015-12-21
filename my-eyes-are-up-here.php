72bcbe319353b67f2d182e02bcadd4425e37ce50
Evgenii Nasyrov <inasyrov@yandex.ru>
2015-12-21 14:42:17 +0000

    Version increment

--- my-eyes-are-up-here.php
+++ my-eyes-are-up-here.php
@@ -3,7 +3,7 @@
  * Plugin Name: My Eyes Are Up Here
  * Plugin URI: https://github.com/interconnectit/my-eyes-are-up-here
  * Description: Detects faces during thumbnail cropping and moves the crop position accordingly.
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: interconnect/it
  * Author URI: http://interconnectit.com
  */
